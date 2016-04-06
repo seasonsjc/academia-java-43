96c5e9c63d35d3b38a7fa419e8db3871cb139886
Eduardo R Carvalho - bisso <ercarval@gmail.com>
2016-04-06 20:07:59 -0300

    meus primeiros passos com java aula 04/04/2016 e introdução a variaveis

---
+++ aj01/Cliente.java
@@ -0,0 +1,14 @@
+class Cliente {
+
+    public static void main (String[] args) {
+        byte idade = 100;
+        long codigoCliente = 1111111111111111111111111111;
+        float valorTotalCompras = 12321232.34f;
+        System.out.println (idade);
+        System.out.println (valorTotalCompras);
+
+        System.out.println ("Xiiiii Marquinhos. ..... não eh que funfou ...!!!! ");
+
+    }
+
+}
---
+++ aj01/PedidoCompra.java
@@ -0,0 +1,9 @@
+class PedidoCompra {
+
+    public static void main (String[] args) {
+
+        System.out.println ("Caraca Maluco ... ;)");
+
+    }
+
+}
