d331721b1482c52eac1a3acee6fb925d52ccb447
Eduardo R Carvalho - bisso <ercarval@gmail.com>
2016-04-06 20:11:22 -0300

    variaveis inteiras primitivas

---
+++ aj01/variaveis/Cliente.java
@@ -0,0 +1,50 @@
+
+
+
+/**
+ * Classe - ClienteEspecial
+ * metodo - imprimirNotaFiscal - ()
+ * metodo - imprimir
+ * varivavel - codigoCliente
+ *
+ */
+class Cliente {
+
+    /**
+     * PRIMITIVAS
+     * NUMERICO
+     *  inteira - byte (8) , short(16), int(32), long(64)
+     *  ponto flutuante - float (32), double (64)
+     *
+     * Caractere
+     *    char (16)
+     *
+     * Booleano
+     *    boolean -
+     */
+    public static void main (String[] args) {
+
+        byte idade = 100;
+
+        byte idadeVovo = idade;
+
+        long codigoCliente = 10000000L;
+
+        float valorTotalCompras = 12321232.34f;
+
+        char sexo = 'M';
+
+        boolean ativo = true ; // false
+
+        String nome = "Bissolino";
+
+
+        System.out.println (idade);
+
+        System.out.println (valorTotalCompras);
+
+        System.out.println ("Xiiiii Marquinhos. ..... não eh que funfou ...!!!! ");
+
+    }
+
+}
