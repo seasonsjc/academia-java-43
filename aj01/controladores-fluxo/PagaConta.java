6818e29cdbac5f4dc4194b9a9751e6b7e15f8402
Eduardo R Carvalho - bisso <ercarval@gmail.com>
2016-04-06 22:58:44 -0300

    controladore de fluxo e operadores com parte do exercicio de operadores

--- aj01/controladores-fluxo/PagaConta.java
+++ aj01/controladores-fluxo/PagaConta.java
@@ -10,11 +10,14 @@ class PagaConta {
         int totalParcelas = 18;
 
         // Saber qual eh o valor de Cada Parcela
+        double valorParcela = valorTotalCompra / totalParcelas;
 
         // Adicionar 0.123 de juros nas parcelas
+        double valorParcelaComJuros = valorParcela * 0.123;
 
         // Imprimir o total + juros do carne
-        
+        double valorParcela = valorTotalCompra / totalParcelas;
+
         // Pagar um numero X de Parcelas
 
         // Avaliar se o carne esta totalmente quitado
